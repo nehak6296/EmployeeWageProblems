5650831560e8a08675af62446aa0e20cecbf1ce3
nehak6296 <nehak6296@gmail.com>
2021-03-03 20:39:23 +0530

    [Neha]Add:Calculating employee wage for month

--- Solution2/EmployeeAttendance/Program.cs
+++ Solution2/EmployeeAttendance/Program.cs
@@ -7,28 +7,33 @@ namespace EmployeeAttendance
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUMBER_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
             int empHrs = 0;
             int empWage = 0;
+            int salary = 0;    
             Random random = new Random();
-            int empCheck = random.Next(0,3);
-            switch (empCheck)
+
+            for (int day = 0; day <= NUMBER_OF_WORKING_DAYS; day++)
             {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                salary = salary + empWage;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-
-            Console.WriteLine("Employee Wage :" + empWage);
-
+            Console.WriteLine("Salary for Month :" + salary);
         }
     }
 }
