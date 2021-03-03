b02436201794dc36d9ec54003e223df0f602f7d2
nehak6296 <nehak6296@gmail.com>
2021-03-03 19:29:15 +0530

    [Neha]Add:Calculating part time employee wage

--- Solution2/EmployeeAttendance/Program.cs
+++ Solution2/EmployeeAttendance/Program.cs
@@ -7,15 +7,25 @@ namespace EmployeeAttendance
         static void Main(string[] args)
         {
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0,2);
             if (empCheck == IS_FULL_TIME)
+            {
                 empHrs = 8;
+            }
             else
+                if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else
+            {
                 empHrs = 0;
+            }
             empWage = empHrs * EMP_RATE_PER_HOUR;
 
             Console.WriteLine("Employee Wage :" + empWage);
